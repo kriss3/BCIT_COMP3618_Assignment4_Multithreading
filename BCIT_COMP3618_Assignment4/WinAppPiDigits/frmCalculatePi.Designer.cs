1b56563a5d5c92e2a3d1a01fef51b8fd7873fd82
Krzysztof Szczurowski <kszczurowski@absolute.com>
2018-02-24 17:25:09 -0800

    Finished organizing controls for the first part of the assingnment 4. Compiles OK.

---
+++ BCIT_COMP3618_Assignment4/WinAppPiDigits/frmCalculatePi.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WinAppPiDigits
+{
+    partial class frmCalculatePi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtBoxPi = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(1, 366);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(567, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(98, 13);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(82, 22);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digits of PI: ";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(202, 8);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 30);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxPi
+            // 
+            this.txtBoxPi.Location = new System.Drawing.Point(12, 44);
+            this.txtBoxPi.Multiline = true;
+            this.txtBoxPi.Name = "txtBoxPi";
+            this.txtBoxPi.Size = new System.Drawing.Size(543, 316);
+            this.txtBoxPi.TabIndex = 4;
+            // 
+            // frmCalculatePi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 390);
+            this.Controls.Add(this.txtBoxPi);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.progressBar);
+            this.Name = "frmCalculatePi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BCIT COMP 3618 Assignment 4 - Krzysztof Szczurowski";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtBoxPi;
+    }
+}
+
--- BCIT_COMP3618_Assignment4/WinAppPiDigits/frmCalculatePi.cs
+++ BCIT_COMP3618_Assignment4/WinAppPiDigits/frmCalculatePi.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WinAppPiDigits
 {
-    public partial class Form1 : Form
+    public partial class frmCalculatePi : Form
     {
-        public Form1()
+        public frmCalculatePi()
         {
             InitializeComponent();
         }
